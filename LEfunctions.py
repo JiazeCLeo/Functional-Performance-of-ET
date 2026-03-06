a6ddb3257244036430a879455b4e8b0f18be7cc9
JiazeCLeo <144848288+JiazeCLeo@users.noreply.github.com>
2026-03-06 16:57:41 +0800

    Add files via upload

--- LEfunctions.py
+++ LEfunctions.py
@@ -52,3 +52,32 @@ def fun_LESFE(df, Qn_col, RH_col, T_col, e_vsat_col, Airpressure_col):
     LESFE = Qn_25 / (Bowen_SFE_25 + 1)
     
     return LESFE
+
+def fun_LESFE_Mod(df, Qn_col, RH_col, T_col, e_vsat_col, Airpressure_col, GH_col):
+
+    # Constants
+    Cp = 1005  # J/(kg K), Specific heat capacity at constant pressure
+    Rv = 500  # Specific gas constant for water vapor (J/(kg K))
+    la = 2 * 10**6  # J/kg, Latent heat of vaporization
+    
+    # Extract columns from the DataFrame
+    Qn_25 = df[Qn_col]
+    T_25 = df[T_col]
+    RH_25 = df[RH_col]
+    e_vsat = df[e_vsat_col]
+    Airpressures = df[Airpressure_col]
+    GH = df[GH_col]
+
+    # Calculate mixing ratio (mvsat)
+    mvsat = 0.622 * e_vsat / (Airpressures - e_vsat)
+
+    # Calculate specific humidity (q_25)
+    q_25 = RH_25 * mvsat / 100
+
+    # Calculate Bowen ratio for SFE
+    Bowen_SFE_25 = (Rv * Cp * (T_25 + 273)**2) / (la**2 * q_25)
+
+    # Calculate LESFE
+    LESFE_Mod = (Qn_25-GH) / (Bowen_SFE_25 + 1)
+    
+    return LESFE_Mod
